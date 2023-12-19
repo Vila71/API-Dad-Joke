79113ba84ba0647efb0cb7b4bc4b46c925b2d058
Vila71 <lolivila71@gmail.com>
2023-12-19 09:19:10 +0100

    Ejercicio Api resuelto

---
+++ script.js
@@ -0,0 +1,22 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch ('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+  console.log("funciona")
+  console.log(data)
+
+  jokeEl.innerHTML = data.joke
+}
+
